5db14daacff9df8c23e18c721d1e61dfc6c13f79
Nathan Bhak <nathanbhak@unknown.home>
2020-08-30 21:03:27 +0100

    Fixing stuff

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, SafeAreaView } from 'react-native'
+
 import CodeEditArea from './app/components/Editor.js'
+import Menu from './app/components/Menu.js'
+
+import cmColors from './app/assets/cmColors.json'
 
 const App = () => {
+  const [theme, setTheme] = useState('eclipse')
+  const [mode, setMode] = useState('javascript')
+
   return (
-    <SafeAreaView style={styles.container}>
-      <CodeEditArea />
+    <SafeAreaView
+      style={[
+        styles.container,
+        { backgroundColor: cmColors.backgroundColor[theme] }
+      ]}
+    >
+      <Menu
+        theme={theme}
+        mode={mode}
+        handleChangeTheme={setTheme}
+        handleChangeMode={setMode}
+      />
+      <CodeEditArea theme={theme} mode={mode} />
     </SafeAreaView>
   )
 }
--- app/components/Menu.js
+++ app/components/Menu.js
@@ -1,6 +1,58 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import { Picker } from '@react-native-community/picker'
 
-const Menu = () => {}
+import cmThemes from '../assets/cmThemes.json'
+import cmModes from '../assets/cmModes.json'
+import cmColors from '../assets/cmColors.json'
+
+const Menu = ({ theme, mode, handleChangeTheme, handleChangeMode }) => {
+  return (
+    <View style={styles.container}>
+      <Picker
+        style={styles.picker}
+        mode="dropdown"
+        selectedValue={theme}
+        onValueChange={handleChangeTheme}
+        itemStyle={{ color: cmColors.color[theme] }}
+      >
+        {Object.keys(cmThemes).map((el) => (
+          <Picker.Item
+            label={el.charAt(0).toUpperCase() + el.slice(1)}
+            value={el}
+            key={el}
+          />
+        ))}
+      </Picker>
+      <Picker
+        style={styles.picker}
+        mode="dropdown"
+        selectedValue={mode}
+        onValueChange={handleChangeMode}
+        itemStyle={{ color: cmColors.color[theme] }}
+      >
+        {Object.keys(cmModes).map((el) => (
+          <Picker.Item
+            label={el.charAt(0).toUpperCase() + el.slice(1)}
+            value={el}
+            key={el}
+          />
+        ))}
+      </Picker>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 0,
+    flexDirection: 'row',
+    alignItems: 'center',
+    top: 15
+  },
+  picker: {
+    flex: 1
+  }
+})
 
 export default Menu
