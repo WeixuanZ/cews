8b919160d6e65648ae1710c9b1d10d0d179d6ded
ss1234 <ss450555@gmail.com>
2020-08-31 18:00:43 +0100

    First version using React Navigation
    Uses react navigation to make split the app into two screens: the Editor and the Settings. A button on the header of editor takes you to settings. In the future I will style the header and also remake the settings page to have a searchable menu.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { StyleSheet, SafeAreaView } from 'react-native'
+import { Button, StyleSheet, SafeAreaView } from 'react-native'
 
 import CodeEditArea from './app/components/Editor.js'
 import Menu from './app/components/Menu.js'
@@ -8,6 +8,8 @@ import cmColors from './app/assets/cmColors.json'
 
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+
+const Stack = createStackNavigator();
 
 const App = () => {
   const [theme, setTheme] = useState('eclipse')
@@ -20,13 +22,26 @@ const App = () => {
         { backgroundColor: cmColors.backgroundColor[theme] }
       ]}
     >
-      <Menu
-        theme={theme}
-        mode={mode}
-        handleChangeTheme={setTheme}
-        handleChangeMode={setMode}
-      />
-      <CodeEditArea theme={theme} mode={mode} />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Editor"  options={({ navigation }) => ({
+              title: 'Editor',
+              headerRight: () => (
+                <Button
+                  onPress={() => navigation.navigate('Settings')}
+                  title="settings"
+                  type="material"
+                />
+              ),
+            })}
+          >
+            {(props) => <CodeEditArea theme={theme} mode={mode} />}
+          </Stack.Screen>
+          <Stack.Screen name="Settings">
+            {(props) => <Menu theme={theme} mode={mode} handleChangeTheme={setTheme} handleChangeMode={setMode} />}
+          </Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaView>
   )
 }
