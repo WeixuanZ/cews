a1c423e0e94bfe42327045a9dda66d8892c03b84
Siddharth Sharma <ss12345@users.noreply.github.com>
2020-09-03 19:24:38 +0100

    feat: Setting menus for theme and language
    Using react navigation to split the app into two screens: the Editor and the Settings. A button on the header of editor takes you to settings. A searchable flat list is implemented for selection.
    
    * Extracted background and text colours from CodeMirror themes into cmColors.json
    * Added prettier
    * Added undo, redo and save buttons to header
    
    
    Co-authored-by: W Zhang <wz315@cam.ac.uk>
    Refs #1, #2, #4

--- app/components/Menu.js
+++ app/components/Menu.js
@@ -1,6 +1,58 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import { View, StyleSheet, TouchableOpacity } from 'react-native'
+import { ListItem } from 'react-native-elements'
 
-const Menu = () => {}
+import colors from '../config/colors.js'
+
+const Menu = ({
+  theme,
+  mode,
+  navigation,
+  handleChangeTheme,
+  handleChangeMode
+}) => {
+  const SettingItem = ({ title, subtitle, target }) => (
+    <TouchableOpacity>
+      <ListItem
+        onPress={() => navigation.navigate(target)}
+        bottomDivider={true}
+      >
+        <ListItem.Content style={styles.listItem}>
+          <ListItem.Title style={styles.listContent}>{title}</ListItem.Title>
+          <ListItem.Subtitle style={styles.listContent}>
+            {subtitle}
+          </ListItem.Subtitle>
+          <ListItem.Chevron color={colors.primary} />
+        </ListItem.Content>
+      </ListItem>
+    </TouchableOpacity>
+  )
+
+  return (
+    <View>
+      <SettingItem
+        title="Theme"
+        subtitle={theme[0].toUpperCase() + theme.slice(1)}
+        target="Set Editor Theme"
+      />
+      <SettingItem
+        title="Language"
+        subtitle={mode[0].toUpperCase() + mode.slice(1)}
+        target="Set Editor Language"
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  listItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  listContent: {
+    flex: 1
+  }
+})
 
 export default Menu
