bfebffeff6123fd2e56eef7d654ef56b54dcc3b2
Siddharth Sharma <ss12345@users.noreply.github.com>
2020-09-03 19:24:38 +0100

    feat: Setting menus for theme and language
    Using react navigation to split the app into two screens: the Editor and the Settings. A button on the header of editor takes you to settings. A searchable flat list is implemented for selection.
    
    * Extracted background and text colours from CodeMirror themes into cmColors.json
    * Added prettier
    * Added undo, redo and save buttons to header
    
    Refs #1, #2, #4
    
    
    Co-authored-by: W Zhang <wz315@cam.ac.uk>

--- App.js
+++ App.js
@@ -1,12 +1,96 @@
-import React from 'react'
-import { StyleSheet, SafeAreaView } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
 import CodeEditArea from './app/components/Editor.js'
+import Menu from './app/components/Menu.js'
+import SearchableList from './app/components/SearchableList.js'
+import { HeaderButtons, Item } from './app/components/HeaderButtons'
+
+import cmModes from './app/assets/cmModes.json'
+import cmColors from './app/assets/cmColors.json'
+
+const Stack = createStackNavigator()
 
 const App = () => {
+  const [theme, setTheme] = useState('eclipse')
+  const [mode, setMode] = useState('javascript')
+
   return (
-    <SafeAreaView style={styles.container}>
-      <CodeEditArea />
-    </SafeAreaView>
+    <View
+      style={[
+        styles.container,
+        { backgroundColor: cmColors.backgroundColor[theme] }
+      ]}
+    >
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Editor"
+            options={({ navigation }) => ({
+              title: '',
+              // eslint-disable-next-line react/display-name
+              headerRight: () => (
+                <HeaderButtons>
+                  <Item
+                    title="undo"
+                    iconName="undo"
+                    onPress={() => console.log('undo')}
+                  />
+                  <Item
+                    title="redo"
+                    iconName="redo"
+                    onPress={() => console.log('redo')}
+                  />
+                  <Item
+                    title="save"
+                    iconName="save"
+                    onPress={() => console.log('save')}
+                  />
+                  <Item
+                    title="settings"
+                    iconName="tune"
+                    onPress={() => navigation.navigate('Settings')}
+                  />
+                </HeaderButtons>
+              )
+            })}
+          >
+            {(props) => <CodeEditArea theme={theme} mode={mode} />}
+          </Stack.Screen>
+          <Stack.Screen name="Settings">
+            {(props) => (
+              <Menu
+                theme={theme}
+                mode={mode}
+                navigation={props.navigation}
+                handleChangeTheme={setTheme}
+                handleChangeMode={setMode}
+              />
+            )}
+          </Stack.Screen>
+          <Stack.Screen name="Set Editor Theme">
+            {(props) => (
+              <SearchableList
+                data={Object.keys(cmColors.backgroundColor)}
+                value={theme}
+                handleChangeValue={setTheme}
+              />
+            )}
+          </Stack.Screen>
+          <Stack.Screen name="Set Editor Language">
+            {(props) => (
+              <SearchableList
+                data={Object.keys(cmModes)}
+                value={mode}
+                handleChangeValue={setMode}
+              />
+            )}
+          </Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
   )
 }
 
