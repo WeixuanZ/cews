e196730e84457dbce1eeed81c16808f1d4d38d16
ss1234 <ss450555@gmail.com>
2020-09-16 19:37:41 +0100

    Dark Mode setting (WIP). Need to fix searchable list since it doesn't change with the dark mode setting.

--- app/components/Menu.js
+++ app/components/Menu.js
@@ -1,25 +1,93 @@
-import React from 'react'
-import { View, StyleSheet, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { View, StyleSheet, TouchableOpacity, Switch } from 'react-native'
+import { DefaultTheme, DarkTheme } from '@react-navigation/native'
 import { ListItem } from 'react-native-elements'
 
 import colors from '../config/colors.js'
 
-const Menu = ({ theme, mode, navigation }) => {
+const Menu = ({
+  theme,
+  mode,
+  navigation,
+  handleChangeTheme,
+  handleChangeNavTheme,
+  handleChangeNavBool,
+  navTheme,
+  isLightMode
+}) => {
+  const toggleSwitch = () => {
+    handleChangeNavBool(!isLightMode)
+    changeTheme(isLightMode)
+  }
+  const changeTheme = (state) => {
+    if (!state) {
+      handleChangeNavTheme(DefaultTheme)
+      if (theme == '3024-night') {
+        handleChangeTheme('eclipse')
+      }
+    } else {
+      handleChangeNavTheme(DarkTheme)
+      if (theme == 'eclipse') {
+        handleChangeTheme('3024-night')
+      }
+    }
+  }
+  const getColor = (color1, color2) => {
+    return isLightMode ? color1 : color2
+  }
   const SettingItem = ({ title, subtitle, target }) => (
     <TouchableOpacity>
       <ListItem
         onPress={() => navigation.navigate(target)}
         bottomDivider={true}
+        containerStyle={{ backgroundColor: getColor('#FFFFFF', '#000000') }}
       >
         <ListItem.Content style={styles.listItem}>
-          <ListItem.Title style={styles.listContent}>{title}</ListItem.Title>
-          <ListItem.Subtitle style={styles.listContent}>
+          <ListItem.Title
+            style={
+              (styles.listContent, { color: getColor('#000000', '#FFFFFF') })
+            }
+          >
+            {title}
+          </ListItem.Title>
+          <ListItem.Subtitle
+            style={
+              (styles.listContent, { color: getColor('#000000', '#FFFFFF') })
+            }
+          >
             {subtitle}
           </ListItem.Subtitle>
           <ListItem.Chevron color={colors.primary} />
         </ListItem.Content>
       </ListItem>
     </TouchableOpacity>
+  )
+
+  const SwitchItem = ({ title, subtitle }) => (
+    <ListItem
+      bottomDivider={true}
+      containerStyle={{ backgroundColor: getColor('#FFFFFF', '#000000') }}
+    >
+      <ListItem.Content style={styles.listItem}>
+        <ListItem.Title
+          style={
+            (styles.listContent, { color: getColor('#000000', '#FFFFFF') })
+          }
+        >
+          {title}
+        </ListItem.Title>
+        <ListItem.Subtitle style={styles.listContent}>
+          {subtitle}
+        </ListItem.Subtitle>
+        <Switch
+          trackColor={{ false: '#767577', true: '#81b0ff' }}
+          thumbColor={isLightMode ? '#f5dd4b' : '#f4f3f4'}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isLightMode}
+        />
+      </ListItem.Content>
+    </ListItem>
   )
 
   return (
@@ -34,6 +102,7 @@ const Menu = ({ theme, mode, navigation }) => {
         subtitle={mode[0].toUpperCase() + mode.slice(1)}
         target="Set Editor Language"
       />
+      <SwitchItem title="Dark/Light" subtitle={''} />
     </View>
   )
 }
